88089549892114381e85525f475f0a9328f8ba8a
Ildar Zagretdinov <ildar.zagretdinov@diabolocom.com>
2024-07-21 22:42:23 +0300

    test

--- test/about.test.tsx
+++ test/about.test.tsx
@@ -6,6 +6,6 @@ describe('About', () => {
     it('Should render author name', () => {
         const { container } = render(<About />);
 
-        expect(container.textContent).toBe('AboutAuthor: [Ildar Zagretdino]');
+        expect(container.textContent).toBe('AboutAuthor: [Ildar Zagretdinov]');
     });
 });
